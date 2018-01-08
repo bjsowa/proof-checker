bdbd8b1563bc557f82277598d8577a6cf34776ab
Błażej Sowa <bsowa123@gmail.com>
2018-01-08 15:35:56 +0100

    print to channel

--- proof.ml
+++ proof.ml
@@ -1,3 +1,5 @@
+open Core
+
 type formula = 
 	| Lit of char
 	| Neg of formula
@@ -15,27 +17,28 @@ type proof =
 	  goal : formula;
 	  proof: premise list; }
 
-open Core
+let equal f1 f2 = 
+	f1 = f2
 
 let rec output_value outc = function
-	{ name; goal; proof } -> printf "goal %s: %a\nproof\n%a\nend." 
+	{ name; goal; proof } -> fprintf outc "goal %s: %a\nproof\n%a\nend." 
 		name formula_value goal proof_value proof
 
 and proof_value outc = function
 	| [] -> ()
-	| (Formula f)::[]  -> printf "%a"
+	| (Formula f)::[]  -> fprintf outc "%a"
 		formula_value f
-	| (Formula f)::t   -> printf "%a;\n%a" 
+	| (Formula f)::t   -> fprintf outc "%a;\n%a" 
 		formula_value f proof_value t
-	| (Frame (f,p))::[] -> printf "[ %a :\n%a ]"
+	| (Frame (f,p))::[] -> fprintf outc "[ %a :\n%a ]"
 		formula_value f proof_value p
-	| (Frame (f,p))::t -> printf "[ %a :\n%a ];\n%a"
+	| (Frame (f,p))::t -> fprintf outc "[ %a :\n%a ];\n%a"
 		formula_value f proof_value p proof_value t
 
 and formula_value outc = function
-	| Lit x     -> printf "%c" x
-	| Neg x     -> printf "~(%a)" formula_value x
-	| And (x,y) -> printf "(%a)/\\(%a)" formula_value x formula_value y
-	| Or (x,y)  -> printf "(%a)\\/(%a)" formula_value x formula_value y
-	| Imp (x,y) -> printf "(%a)=>(%a)" formula_value x formula_value y
-	| Eq (x,y)  -> printf "(%a)<=>(%a)" formula_value x formula_value y
+	| Lit x     -> fprintf outc "%c" x
+	| Neg x     -> fprintf outc "~(%a)" formula_value x
+	| And (x,y) -> fprintf outc "(%a)/\\(%a)" formula_value x formula_value y
+	| Or (x,y)  -> fprintf outc "(%a)\\/(%a)" formula_value x formula_value y
+	| Imp (x,y) -> fprintf outc "(%a)=>(%a)" formula_value x formula_value y
+	| Eq (x,y)  -> fprintf outc "(%a)<=>(%a)" formula_value x formula_value y
