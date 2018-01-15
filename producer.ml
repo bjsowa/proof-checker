25fdf7da2b118e0dd05f50bd3c619a952e00420b
Błażej Sowa <bsowa123@gmail.com>
2018-01-15 06:36:54 +0100

    add equivalence rules

--- producer.ml
+++ producer.ml
@@ -25,16 +25,44 @@ let orE premises form = match form with
 	| Imp(a,b) ->
 		let imA = FormulaSet.filter_map premises
 			~f:(function
-				| Imp(c,d) -> if d = b then Some c else None
+				| Imp(c,d) -> 
+					if d = b then Some c else None
 				| _ -> None)
 		and orA = FormulaSet.filter_map premises
 			~f:(function
-				| Or(c,d) -> if c = a then Some d 
-							 else if d = a then Some c
-							 else None
+				| Or(c,d) -> 
+					if c = a then Some d 
+					else if d = a then Some c
+					else None
 				| _ -> None) in
 		if not (FormulaSet.is_empty @@ FormulaSet.inter imA orA)
 		then FormulaSet.singleton b
+		else FormulaSet.empty
+	| _ -> FormulaSet.empty
+
+let eqE premises form = match form with
+	| Eq(a,b) -> 
+		FormulaSet.filter_map premises
+			~f:(function
+				| Imp(c,d) -> 
+					if c = a then Some(Imp(b,d))
+					else if c = b then Some(Imp(a,d))
+					else None
+				| _ -> None)
+	| Imp(a,b) ->
+		FormulaSet.filter_map premises
+			~f:(function
+				| Eq(c,d) ->
+					if c = a then Some(Imp(d,b))
+					else if d = a then Some(Imp(c,b))
+					else None
+				| _ -> None)
+	| _ -> FormulaSet.empty
+
+let eqI premises form = match form with
+	| Imp(a,b) ->
+		if FormulaSet.mem premises (Imp(b,a))
+		then FormulaSet.of_list [Eq(a,b);Eq(b,a)]
 		else FormulaSet.empty
 	| _ -> FormulaSet.empty
 
@@ -42,7 +70,9 @@ let produce premises form =
 	andE form $@
 	impEl premises form $@
 	impEr premises form $@
-	orE premises form
+	orE premises form $@
+	eqE premises form $@
+	eqI premises form
 
 let check_introduction premises form = match form with
 	| And(a,b) ->
