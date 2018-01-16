1c87b01228cdd8ca11fbc23b4bb77bb40c034587
Błażej Sowa <bsowa123@gmail.com>
2018-01-16 15:11:09 +0100

    add parse only flag

--- main.ml
+++ main.ml
@@ -3,19 +3,22 @@ open Lexing
 open Checker
 open Utilities
 
-let main filename () =
+let main filename parse () =
     let inx = In_channel.create filename in
     let lexbuf = Lexing.from_channel inx in
     lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
-    
-    let proofs = parse_with_error lexbuf in
-    List.iter proofs ~f:(fun p -> check_proof p);
 
+    let proofs = parse_with_error lexbuf in
+    (if parse 
+    then List.iter proofs ~f:(printf "%a\n" Proof.output_value)
+    else List.iter proofs ~f:(check_proof));
     In_channel.close inx
 
 let () =
     Command.basic 
         ~summary:"Check the correctness of proof of classical logic formula in the natural deduction system"
-        Command.Spec.(empty +> anon ("filename" %: file))
+        Command.Spec.(empty 
+            +> anon ("filename" %: file)
+            +> flag "-parse" no_arg ~doc:"only print parsed proofs")
         main 
     |> Command.run
