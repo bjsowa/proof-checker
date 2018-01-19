3a921f1e0de94c1c8c82fcab2a6d3e6dbe9c5db1
Błażej Sowa <bsowa123@gmail.com>
2018-01-19 15:54:34 +0100

    add fill_production function

--- checker.ml
+++ checker.ml
@@ -3,35 +3,44 @@ open Proof
 open Producer
 open Utilities
 
-let rec frame premises products = function
+let fill_production premises products goal = function
+	| Some x -> 
+		printf "trying to fill production of formula %a\n" print_formula goal;
+		true
+	| None -> false
+
+let rec frame premises products fill = function
 	| [Formula (f,l)] -> 
 		if FormulaSet.mem premises (f,None) ||
 		   FormulaSet.mem products (f,None) ||
-		   check_introduction premises f
+		   check_introduction premises f ||
+		   fill_production premises products f fill
 		then f
 		else raise (ProofError (f,l))
 	| (Formula (f,l))::t -> 
 		if FormulaSet.mem premises (f,None)
-		then frame premises products t
-		else if FormulaSet.mem products (f,None) || check_introduction premises f 
+		then frame premises products fill t
+		else if FormulaSet.mem products (f,None) || 
+			check_introduction premises f ||
+			fill_production premises products f fill
 		then frame 
 			(FormulaSet.add premises (f,None)) 
 			((FormulaSet.remove products (f,None)) $@ (produce premises f)) 
-			t
+			fill t
 		else raise (ProofError (f,l))
 	| (Frame (f,p))::t ->
 		let goal = frame 
 			(FormulaSet.add premises (f,None)) 
 			((FormulaSet.remove products (f,None)) $@ (produce premises f)) 
-			p in
+			fill p in
 		let newf = Imp(f,goal) in
-		frame premises (FormulaSet.add products (newf,None)) t
+		frame premises (FormulaSet.add products (newf,None)) fill t
 	| _ -> raise FrameError
 
 let check_proof p fill = match p with
 	{ name; goal; proof } -> 
 	try 
-		let res = frame FormulaSet.empty FormulaSet.empty proof in
+		let res = frame FormulaSet.empty FormulaSet.empty fill proof in
 		if Formula.compare_formula res goal = 0
 		then printf "goal %s: proved successfully\n" name
 		else eprintf "goal %s: not proved\n" name
